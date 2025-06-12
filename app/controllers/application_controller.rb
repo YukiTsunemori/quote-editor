bb8b035872bfb215cfc7be2135aa9927253c3ad9
yuki.tsunemori <yuki.tsunemori1229@gmail.com>
2025-06-12 21:16:04 +0900

    Turbo_frame_tag as of now(reset index page at next step)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  before_action -> { sleep 0.5 }
+  # before_action -> { sleep 0.5 }
   # turbo driveを使用すると、プログレスバーがオフになるため手動で設定する必要がある。
   # sleep 0.5秒でプログレスバーを表示させる。
 end
