c992bbd1ba98b4bb77227673819e187d3795cb16
yuki.tsunemori <yuki.tsunemori1229@gmail.com>
2025-06-14 01:17:32 +0900

    Add orderd scope at quote.rb

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,3 +1,8 @@
 class Quote < ApplicationRecord
   validates :name, presence: true
+
+  scope :ordered, -> { order(id: :desc) }
+  # orderedというクラスメソッド(スコープ)を定義する。
+  # このメソッドが呼び出されると、order(id: :desc)というActive Recordクエリが実行され、
+  # DBから取得されるレコードがIDの降順にソートされる！
 end
