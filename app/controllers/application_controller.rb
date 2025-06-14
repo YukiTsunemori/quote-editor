aaff69c37ffe14f980e44eda337c196e908997a0
yuki.tsunemori <yuki.tsunemori1229@gmail.com>
2025-06-14 16:45:48 +0900

    add navbar at top

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,9 @@ class ApplicationController < ActionController::Base
   # before_action -> { sleep 0.5 }
   # turbo driveを使用すると、プログレスバーがオフになるため手動で設定する必要がある。
   # sleep 0.5秒でプログレスバーを表示させる。
+
+  include Devise::Controllers::Helpers
+
   before_action :authenticate_user!, unless: :devise_controller?
   # authenticate_user!メソッドは、ユーザーがログインしているかどうかを確認し、ログインしていない場合はログインページにリダイレクトします。
   # unless: :devise_controller?は、未認証のユーザーがログインフォームにアクセスできるようにする。
