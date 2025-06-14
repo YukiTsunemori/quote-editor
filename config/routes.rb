4c4c02e2118f0a6d4ea819b166947a230c8b4894
yuki.tsunemori <yuki.tsunemori1229@gmail.com>
2025-06-14 17:00:32 +0900

    devise_for :user at routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  # devise_for :users
+  devise_for :users
+  # devise_forはauthenticate_user!やdevise_controller?ヘルパーメソッドを生成する
+  # Railsアプリケーション全体で、devise認証機能が利用可能になるための1行。
   resources :quotes
   root to: "pages#home"
 end
