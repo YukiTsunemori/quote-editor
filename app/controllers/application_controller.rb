5da78d61a444dfc562acb3cd2d4eed70f30fb64b
yuki.tsunemori <yuki.tsunemori1229@gmail.com>
2025-06-14 16:29:32 +0900

    page controller & before_action of login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,7 @@ class ApplicationController < ActionController::Base
   # before_action -> { sleep 0.5 }
   # turbo driveを使用すると、プログレスバーがオフになるため手動で設定する必要がある。
   # sleep 0.5秒でプログレスバーを表示させる。
+  before_action :authenticate_user!, unless: :devise_controller?
+  # authenticate_user!メソッドは、ユーザーがログインしているかどうかを確認し、ログインしていない場合はログインページにリダイレクトします。
+  # unless: :devise_controller?は、未認証のユーザーがログインフォームにアクセスできるようにする。
 end
---
+++ app/controllers/page_controller.rb
@@ -0,0 +1,7 @@
+class PageController < ApplicationController
+  skip_before_action :authenticate_user!
+
+  def home
+    
+  end
+end
