f610f9b6c754d389717347dcde9368f43b7bc5f8
yuki.tsunemori <yuki.tsunemori1229@gmail.com>
2025-06-17 22:44:55 +0900

    edit broadcasts method add company

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -15,7 +15,7 @@ class Quote < ApplicationRecord
   # after_update_commit -> { broadcast_replace_later_to "quotes" }
   # after_destroy_commit -> { broadcast_remove_to "quotes" }
 
-  broadcasts_to ->(quote) { "quotes" }, inserts_by: :prepend
+  broadcasts_to ->(quote) { [ quote.company, "quotes" ] }, inserts_by: :prepend
   # railsはこのメソッド1行で、上に書いた3行のコードを実行する。
   # broadcasts_toメソッドは、RailsのAction Cableを使用して、リアルタイムでの更新を可能にする。
   # 6章にて、
