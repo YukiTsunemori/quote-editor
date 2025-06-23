67c5eac5ce815f6ae6a322cb25e64f768dab6019
yuki.tsunemori <yuki.tsunemori1229@gmail.com>
2025-06-23 20:54:50 +0900

    create set_line_item_date

--- app/controllers/line_item_dates_controller.rb
+++ app/controllers/line_item_dates_controller.rb
@@ -1,6 +1,7 @@
 class LineItemDatesController < ApplicationController
   # 各アクションの実行前にset_quoteメソッドを実行
   before_action :set_quote
+  before_action :set_line_item_date, only: [ :edit, :update, :destroy ]
   def new
     # 現在のquoteに紐づく新しいline_item_dateオブジェクトをビルド
     @line_item_date = @quote.line_item_dates.build
@@ -17,7 +18,28 @@ class LineItemDatesController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @line_item_date.update(line_item_date_params)
+      redirect_to quote_path(@quote), notice: "Date was successfully updated."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @line_item_date.destroy
+    redirect_to quote_path(@quote), notice: "Date was successfully destroyed."
+  end
+
   private
+
+  # edit, update, destroyアクションの前に特定のline_item_dateを探す
+  def set_line_item_date
+    @line_item_date = @quote.line_item_dates.find(params[:id])
+  end
 
   def line_item_date_params
     params.require(:line_item_date).permit(:date)
