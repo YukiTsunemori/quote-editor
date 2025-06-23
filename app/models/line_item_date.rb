c95c670d35dda395b2c9249c7955b3cbe1b4d471
yuki.tsunemori <yuki.tsunemori1229@gmail.com>
2025-06-23 19:29:21 +0900

    Create! db LimeItemDate model

---
+++ app/models/line_item_date.rb
@@ -0,0 +1,10 @@
+class LineItemDate < ApplicationRecord
+  # このLineItemDateがどの見積もりに属するかを定義
+  belongs_to :quote
+
+  # 日付が必須であることと、見積もり内で日付が重複しないことを設定する
+  validates :date, prpesence: true, uniqueness: { scope: :quote_id }
+  # このscopeはquote_idが同じ日付を複数持つことを許さないと言う意味である。
+
+  scope :ordered, -> { order(date: :asc) }
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   # devise_forはauthenticate_user!やdevise_controller?ヘルパーメソッドを生成する
   # Railsアプリケーション全体で、devise認証機能が利用可能になるための1行。
-  resources :quotes
+  resources :quotes do
+    resources :line_item_dates, except: [ :index, :show ]
+    # indexアクションは全ての日付がすでにshowページに表示されるため、必要ない。
+    # showアクションはここの日付明細だけを見ることに意味がないため必要ない。
+  end
   root to: "pages#home"
 end
